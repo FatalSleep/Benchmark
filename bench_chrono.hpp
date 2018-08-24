d902ade10c028061a887bd85e74950029b34ced7
Alexander Griffis <fatalsleep@gmail.com>
2018-08-24 09:05:18 -0700

    Added header definitions.

--- bench_chrono.hpp
+++ bench_chrono.hpp
@@ -1,12 +1,15 @@
-#include <chrono>
+#ifndef BENCHMARK_CHRONO
+#define BENCHMARK_CHRONO
+	#include <chrono>
 
-namespace std {
-    namespace chrono {
-        // Returns the current time for benchmarking as type std::chrono::T.
-        template<typename T>
-        uint64_t now() {
-            return std::chrono::duration_cast<T>
-                (std::chrono::high_resolution_clock::now().time_since_epoch()).count();
-        }
-    }
-};
+	namespace std {
+		namespace chrono {
+			// Returns the current time for benchmarking as type std::chrono::T.
+			template<typename T>
+			uint64_t now() {
+				return std::chrono::duration_cast<T>
+					(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+			}
+		}
+	};
+#endif // !BENCHMARK_CHRONO
